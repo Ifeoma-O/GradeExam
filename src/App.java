2de4db1c8fe15c365e2def9db2d8662d7a2f5429
unknown <ifyokpara01@gmail.com>
2024-04-10 13:04:19 +0100

    Updated version

--- src/App.java
+++ src/App.java
@@ -1,3 +1,7 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class App {
     public static void main(String[] args) {
         char[] keys = {'D','B','D','C','C','D','A','E','A','D'};
@@ -11,14 +15,25 @@ public class App {
             {'B', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
             {'E', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'}
         };
+
+        int[][] correct = new int[answers.length][2];
+
         for(int i=0; i<answers.length; i++){
-            int correct = 0;
             for (int j = 0; j<answers[i].length; j++){
                 if (answers[i][j] == keys[j]) {
-                    correct++;
+                    correct[i][0] = i;
+                    correct[i][1]++;
                 }
             }
-            System.out.println("Student " + i + "'s correct answers are " + correct);
+            
         }
+
+        Arrays.sort(correct, Comparator.comparingInt(a -> a[1]));
+        for(int i = 0; i < correct.length; i++){
+            System.out.println("Student " + correct[i][0] + "'s correct count is " + correct[i][1]);
+        }
+        
     }
+
+    
 }
